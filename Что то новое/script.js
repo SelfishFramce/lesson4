78e4f7b7524bf908dc9a38586d6cac2cece6cab7
SelfishFramce <kotpinki1020@gmail.com>
2024-06-23 12:45:22 +0300

    .

---
+++ Что то новое/script.js
@@ -0,0 +1,18 @@
+console.log("Я загадал число от 1 до 10. Попробуй его отгадать!");
+let secretNumber = Math.floor(Math.random() * 10 + 1);
+let attempts = 3;
+
+while(attempts > 0) {
+    let userNumber = Number(prompt("Введите число: "));
+
+    if (userNumber < secretNumber) {
+        console.log("Секретное число больше");
+        attempts = attempts - 1;
+    } else if (userNumber === secretNumber) {
+        console.log("Вы угадали");
+        break
+    } else {
+        console.log("Секретное число меньше");
+        attempts = attempts - 1;
+    }
+}
