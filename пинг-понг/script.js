252a8f4b70c03160bbfafe68251d339a4d4d80d3
SelfishFramce <kotpinki1020@gmail.com>
2024-07-05 11:06:14 +0300

    пинг-понг 2.0

--- пинг-понг/script.js
+++ пинг-понг/script.js
@@ -1,13 +1,19 @@
-const map = document.querySelector("#game");
-const canvas = map.getContext("2d");
-canvas.fillStyle = "rgb(228, 164, 87)";
+const map = document.querySelector('#game');
+const canvas = map.getContext('2d');
+canvas.fillStyle = 'rgb(228, 164, 87)';
 
 const grid = 15;
 const paddleHeight = grid * 5;
 const maxPaddleY = map.height - grid - paddleHeight;
 
-let ballSpeed = 5;
-let paddleSpeed = 2;
+
+
+let ballSpeed = 7;
+let paddleSpeed = 7;
+
+const leftCounter = document.querySelector("#leftCounter");
+
+const rightCounter = document.querySelector("#rightCounter"); 
 
 const leftPaddle = {
     x: grid * 2,
@@ -30,9 +36,8 @@ const ball = {
     y: map.height / 2,
     width: grid,
     height: grid,
-    dy: ballSpeed,
+    dx: ballSpeed,
     dy: -ballSpeed,
-    resetting: false,
     isResetted: false,
 }
 
@@ -57,13 +62,13 @@ function renderRightPaddle() {
     canvas.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
 }
 
-function renderBall() {
-    canvas.fillRect(ball.x, ball.y, ball.width, ball.height);
-}
-
 function movePaddles() {
     leftPaddle.y += leftPaddle.dy;
     rightPaddle.y += rightPaddle.dy;
+}
+
+function renderBall() {
+    canvas.fillRect(ball.x, ball.y, ball.width, ball.height);
 }
 
 function moveBall() {
@@ -71,27 +76,122 @@ function moveBall() {
     ball.y += ball.dy;
 }
 
-function loop () {
+function resetGame() {
+    if ((ball.x < 0 || ball.x > map.width) && !ball.isResetted) {
+        
+        if (ball.x < 0) {
+            leftCounter.textContent = 0;
+        } else if (ball.x > map.width) {
+            rightCounter.textContent = 0;
+        }
+
+        ball.isResetted = true;
+        setTimeout(() => {
+            ball.x = map.width / 2;
+            ball.y = map.height / 2;
+            ball.isResetted = false;
+        }, 1000);
+    }
+}
+
+function collideWallsWithPaddle(paddle) {
+    if (paddle.y < grid) {
+        paddle.y = grid;
+    }
+    else if (paddle.y > maxPaddleY) {
+        paddle.y = maxPaddleY;
+    }
+}
+
+function collideWallsWithPaddles() {
+    collideWallsWithPaddle(leftPaddle);
+    collideWallsWithPaddle(rightPaddle);
+}
+
+function collideWallsWithBall() {
+    if (ball.y < grid) {
+        ball.y = grid;
+        ball.dy = -ball.dy;
+    }
+    else if (ball.y > map.height - grid) {
+        ball.y = map.height - grid;
+        ball.dy = -ball.dy;
+    }
+}
+
+function isCollides(object1, object2) {
+    const width1 = object1.x + object1.width;
+    const width2 = object2.x + object2.width;
+    const height1 = object1.y + object1.height;
+    const height2 = object2.y + object2.height;
+    return object1.x < width2
+        && object2.x < width1
+        && object1.y < height2
+        && object2.y < height1;
+}
+
+function collidePaddlesWithBall() {
+    if (isCollides(ball, rightPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = rightPaddle.x - ball.width;
+        rightCounter.textContent = parseInt(rightCounter.textContent) +1;
+    }
+    else if (isCollides(ball, leftPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = leftPaddle.x + leftPaddle.width;
+        leftCounter.textContent = parseInt(leftCounter.textContent) +1;
+    }
+}
+
+function aiControl() {
+    let direction = 0;
+
+    if (ball.y < rightPaddle.y) {
+        direction = -1;
+    }
+    else if (ball.y > rightPaddle.y + paddleHeight) {
+        direction = 1;
+    }
+
+    rightPaddle.y += paddleSpeed * direction;
+}
+
+
+function loop() {
     clearMap();
+
     renderLeftPaddle();
     renderRightPaddle();
+    aiControl(); // 24
     movePaddles();
+
+    collideWallsWithPaddles(); // 24
+
+    renderBall();
+    moveBall();
+
+    collideWallsWithBall(); // 24
+    collidePaddlesWithBall(); // 24
+
+    resetGame(); // 24
     renderMap();
     requestAnimationFrame(loop);
 }
 
-document.addEventListener("keydown", event => {
-    if (event.key === "w" || event.key === "ц") {
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'w' || event.key === 'ц') {
         leftPaddle.dy = -paddleSpeed;
-    } else if (event.key === "s" || event.key === "ы") {
+    }
+    else if (event.key === 's' || event.key === 'ы') {
         leftPaddle.dy = paddleSpeed;
     }
 });
-
-document.addEventListener("keyup", event => {
-    if (event.key === "w" || event.key === "s" || event.key === "ц" || event.key === "ы") {
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'w' || event.key === 's' || event.key === 'ы' || event.key === 'ц') {
         leftPaddle.dy = 0;
     }
-});
+})
+
 
 requestAnimationFrame(loop);
