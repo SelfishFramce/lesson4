936f33720492c064b1ad987b30a80eaa8650bbe8
SelfishFramce <kotpinki1020@gmail.com>
2024-07-10 15:38:09 +0300

    Игра в пары 1.1

--- Couples/index.js
+++ Couples/index.js
@@ -1,10 +1,24 @@
-import { createBoard } from './scripts/createBoard.js';
+function createBoard(columns) {
+    for (let i = 0; i < count; i++) {
+        gameTable.append(createCard());
+    }
+    const gameTable = template.querySelector(".table");
+  
+    gameTable.style = `
+      grid-template-columns: repeat(${columns}, 1fr);
+      grid-template-rows: repeat(${columns}, 1fr);
+    `;
+}
+
+function createCard(card) {
+
+}
 
 const startButton = document.querySelector('.board__button');
 
 startButton.addEventListener("click", (event) => {
     event.preventDefault()
-    const input = document.querySelector('.board__input');
+    const input = document.querySelector(".board__input");
 
     let columns = input.value;
     let count;
@@ -12,10 +26,11 @@ startButton.addEventListener("click", (event) => {
     if (columns >= 2 && columns <= 6 && columns % 2 == 0) {
         count = columns * columns;
     } else {
-        alert("Нужно написать четное число в указанном диапазоне.");
+        alert("Нужно написать четное число в указанном диапазоне!");
         return;
     }
 
     createBoard(count, columns);
-});
+})
+    
 
